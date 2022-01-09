44961514b5a8cef06f4edefa95e0eb54894f5df0
mikeymanoguerra <mikeymanoguerra@gmail.com>
2022-01-09 11:55:53 -0500

    netlify cms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path:'', loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule)},
   { path:'about', loadChildren: () => import('./pages/about-page/about-page.module').then(m => m.AboutPageModule)},
   { path:'murals', loadChildren: () => import('./pages/murals-page/murals-page.module').then(m => m.MuralsPageModule)},
+  { path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then((m) => m.AdminModule) },
   { path:'trees/:slug', component: PlaceholderComponent},
   { path:'species/:slug', component: PlaceholderComponent}
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'art-and-arbor';
+  isAdminPage$: Observable<boolean>;
+
+  constructor(private _router: Router) { }
+
+  ngOnInit() {
+    this.isAdminPage$ = this._router.events.pipe(
+      filter((evt: any) => {
+        return evt instanceof NavigationEnd;
+      }),
+      map((evt: NavigationEnd) => {
+        return evt.url.includes('/admin');
+      }),
+    );
+  }
 }
